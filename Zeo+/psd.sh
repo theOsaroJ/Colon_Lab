44125865991e8aa0407f6f426df80d01fc49937d
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2022-07-20 12:36:45 -0400

    Update and rename run.sh to psd.sh
    To calculate and plot PSD using helium as probe

--- Zeo+/psd.sh
+++ Zeo+/psd.sh
@@ -2,22 +2,22 @@
 #$ -q hpc@@colon
 #$ -pe smp 8
 #$ -N PSD
-#$ -t 1-3
+#$ -t 1-12
 
 # The MOFs cif files
-cifs=(Cu-BTC.cif NU-800.cif DUT-32.cif)
+cifs=(Cu-BTC.cif NU-800.cif DUT-32.cif DUT-49.cif IRMOF-1.cif IRMOF-10.cif IRMOF-16.cif UIO-66.cif MgMOF-74.cif ZIF-8.cif PCN-61.cif MOF-177.cif)
 
 # Creating a dir for each cif file
 dir=DIR.${cifs[ $SGE_TASK_ID - 1]}
 mkdir $dir
 
 # copying network + job particular cif file into its dir.
-cp network ${cifs[ $SGE_TASK_ID - 1]} $dir
+cp network plot.py ${cifs[ $SGE_TASK_ID - 1]} $dir
 cd $dir
 
 # running the job from each directory.
 
-./network -ha -psd 1.4 1.4 10 ${cifs[ $SGE_TASK_ID - 1]}
+./network -ha -psd 1.4 1.4 50000 ${cifs[ $SGE_TASK_ID - 1]}
 
 # extracting bin and derivative_dist from results
 cat *psd_histo > histo
@@ -29,4 +29,11 @@ awk 'NR>11 {print}' histo | awk -F' ' '{print $4}' > dd
 echo -e "bin, derivative_dist" > Finaldata.csv
 paste binn dd -d"," >> Finaldata.csv
 rm binn dd histo
+
+#Plotting the results
+module load python
+
+python3 plot.py
+
+module unload python
 cd ../
