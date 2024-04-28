1bf60779dcbf869b44bee0461c7ff20e4fd59c7e
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2024-04-28 16:54:39 -0400

    Update lammpsdata.tcl

--- DiffusivityInMOFs/BuildDataFiles/lammpsdata.tcl
+++ DiffusivityInMOFs/BuildDataFiles/lammpsdata.tcl
@@ -1,10 +1,11 @@
+mol load xyz molecules.xyz
 package require topotools
 topo retypebonds
 topo guessangles
 topo retypedihedrals
-molinfo top set a 25.832
-molinfo top set b 25.832
-molinfo top set c 25.832
+molinfo top set a 26.343
+molinfo top set b 26.343
+molinfo top set c 26.343
 molinfo top set alpha 90
 molinfo top set beta 90
 molinfo top set gamma 90
